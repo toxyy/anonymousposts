0d3f0b4d15755033f58e17a38563a823683e597b
Alec <arr70017@uga.edu>
2018-10-02 23:37:29 -0400

    add turkish language

---
+++ language/tr/common.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* 
+* phpBB Extension - toxyy Anonymous Posts
+* @copyright (c) 2018 toxyy <thrashtek@yahoo.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+* 
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+$lang = array_merge($lang, [
+	'ANP_MESSAGE'       => 'Anonim Gönderi',
+	'ANP_ACTION'        => 'Anonim olarak Gönder',
+        'ANP_PERMISSIONS'   => 'Anonim olarak gönderebilir',
+]);
