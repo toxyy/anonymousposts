58ea31751f588a788280fd86cda98faf7022aad5
max-ima <96241406+max-ima@users.noreply.github.com>
2023-10-23 06:48:03 +0200

    Slovenian translation (#29)
    * Slovenian translation
    
    Slovenian - sl

---
+++ language/sl/common.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* @package phpBB Extension - Anonymous Posts
+* @copyright (c) 2018 toxyy <thrashtek@yahoo.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+* Slovenian Translation - Marko K.(max, max-ima,...)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+$lang = array_merge($lang, [
+	'ANP_DEFAULT'				=> 'Anonymous/<br>Anonimno',
+	'ANP_MESSAGE'				=> 'Anonimna objava',
+	'ANP_ACTION'				=> 'Objavi anonimno',
+	'ANP_PERMISSIONS'			=> 'Lahko objavi anonimno',
+	'ANP_EDIT_PERMISSIONS'		=> 'Lahko ureja anonimni status',
+]);
