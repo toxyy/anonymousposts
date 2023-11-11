217bf3d741f1cebc6e3d37388bfe3afa7ebd61a6
toxyy <thrashtek@gmail.com>
2023-11-10 20:01:09 -0500

    Fix Slovenian translation

--- language/sl/common.php
+++ language/sl/common.php
@@ -19,7 +19,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'ANP_DEFAULT'				=> 'Anonymous/<br>Anonimno',
+	'ANP_DEFAULT'				=> 'Anonimno',
 	'ANP_MESSAGE'				=> 'Anonimna objava',
 	'ANP_ACTION'				=> 'Objavi anonimno',
 	'ANP_PERMISSIONS'			=> 'Lahko objavi anonimno',
